1be55600b4800552028cf0e527d285c81edad9f3
haythemGHA <haithem.gharbi@esprit.tn>
2020-10-30 22:32:25 +0100

    ajout

--- main.c
+++ main.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
+#include "fonction.h"
 int main()
 {
-int i,j;
-i=10;
-j=3;
-printf("i=%d j=%d",i,j);
+int a;
+int b;
+printf("\nSaisie du Premier Nombre: ");
+scanf("%d",&a);
+do{
+printf("\nsaisie du Second Nombre: ");
+scanf("%d",&b);
+}while(b==0);
+printf("\n%d+%d=%d",a,b,sommer(a,b));
+printf("\n%d-%d=%d",a,b,soustraire(a,b));
+printf("\n%d/%d=%f",a,b,diviser(a,b));
+return(0);
+
 }
